fbfa022fcee03371742cc3f4484c07d7bb4fc413
Tomohisa Kuranari <tomohisa.kuranari@gmail.com>
2026-09-06 11:24:01 +0900

    Add Section 2.2.1

---
+++ chapter2.2.1.rkt
@@ -0,0 +1,25 @@
+;; 開始: 2026-09-06(Sun) 11:19:19
+;; 終了: 2026-09-06(Sun) 11:23:49
+
+#lang racket
+(require racket/trace)
+
+;; 2.2.1 列の表現 (Representing Sequences)
+
+(define (list-ref items n)
+  (if (= n 0)
+      (car items)
+      (list-ref (cdr items) (- n 1))))
+(define squares (list 1 4 9 16 25))
+(list-ref squares 3)
+
+(define odds (list 1 3 5 7))
+(length odds)
+
+(define (append list1 list2)
+  (if (null? list1)
+      list2
+      (cons (car list1) (append (cdr list1) list2))))
+
+(append squares odds)
+(append odds squares)
